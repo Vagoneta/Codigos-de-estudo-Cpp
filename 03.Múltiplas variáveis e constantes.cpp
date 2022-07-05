4985a862c297f669736d36b27ff87638b0acec77
Vagoneta <103613467+Vagoneta@users.noreply.github.com>
2022-07-05 08:34:06 -0300

    Add files via upload

---
+++ 03.Múltiplas variáveis e constantes.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+#define pi 3.1415
+#define prog cout << "The Vagoneta\n";
+int main(){
+	
+	int hp, poder, vidas;
+	
+	cout << pi << "\n";
+	prog
+	
+	return 0;
+	
+}
